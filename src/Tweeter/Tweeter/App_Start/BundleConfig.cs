5334109c86be9c963b28009acabccc81f087f438
Chase Blazek <chaseblazek@yahoo.com>
2013-11-20 21:17:11 -0600

    Added password strength checker

--- src/Tweeter/Tweeter/App_Start/BundleConfig.cs
+++ src/Tweeter/Tweeter/App_Start/BundleConfig.cs
@@ -16,7 +16,8 @@ namespace Tweeter
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/jquery.validate*",
+                        "~/Scripts/jquery.passstrength*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
