dbca90f79eb2d8fe3e4e1994dd95d09e49ff2647
afrodyma <alex.frodyma@hotmail.com>
2013-11-21 23:12:34 -0600

    Added post controller
    I added the post controller then spent some time slamming my head
    against the wall when trying to view the list of posts. I went to the
    /Posts/Index page which fails because:
    The model backing the 'PostContext' context has changed since the
    database was created. Consider using Code First Migrations to update the
    database (http://go.microsoft.com/fwlink/?LinkId=238269)
    After following the link, it looks like our migration scaffolding is
    worthless compared to what was automatically generated for their
    tutorial. I have no solution for this problem.

---
+++ src/Tweeter/Tweeter/Controllers/PostController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tweeter.Models;
+
+namespace Tweeter.Controllers
+{
+    public class PostController : Controller
+    {
+        private PostContext db = new PostContext();
+
+        //
+        // GET: /Post/
+
+        public ActionResult Index()
+        {
+            return View(db.Posts.ToList());
+        }
+
+        //
+        // GET: /Post/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Post post = db.Posts.Find(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
+
+        //
+        // GET: /Post/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Post/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Posts.Add(post);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(post);
+        }
+
+        //
+        // GET: /Post/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Post post = db.Posts.Find(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
+
+        //
+        // POST: /Post/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(post).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(post);
+        }
+
+        //
+        // GET: /Post/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Post post = db.Posts.Find(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
+
+        //
+        // POST: /Post/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Post post = db.Posts.Find(id);
+            db.Posts.Remove(post);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
