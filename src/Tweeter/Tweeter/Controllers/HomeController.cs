c66b329941a79d9c05abb093cc953be5f9f6fcfb
afrodyma <alex.frodyma@hotmail.com>
2013-11-26 20:46:57 -0600

    Improved home page
    The home page has been improved to display relevant titles and a list of
    all posts. It will also need followed posts and liked posts, but the
    follow and like features have not been implemented yet.

--- src/Tweeter/Tweeter/Controllers/HomeController.cs
+++ src/Tweeter/Tweeter/Controllers/HomeController.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Tweeter.Models;
 
 namespace Tweeter.Controllers
 {
     public class HomeController : Controller
     {
+        private PostContext db = new PostContext();
+
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
-
-            return View();
+            ViewBag.Message = "Welcome to HAL Tweeter!";
+            //ViewBag.PostList = db.Posts.ToList();
+            return View(db.Posts.ToList());
         }
 
         public ActionResult About()
--- src/Tweeter/Tweeter/Filters/InitializeSimpleMembershipAttribute.cs
+++ src/Tweeter/Tweeter/Filters/InitializeSimpleMembershipAttribute.cs
@@ -38,7 +38,7 @@ namespace Tweeter.Filters
                         }
                     }
 
-                    WebSecurity.InitializeDatabaseConnection("DefaultConnection", "UserProfile", "UserId", "UserName", autoCreateTables: true);
+                    //WebSecurity.InitializeDatabaseConnection("DefaultConnection", "UserProfile", "UserId", "UserName", autoCreateTables: true);
                 }
                 catch (Exception ex)
                 {
